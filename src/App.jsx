34df8dbe069502a1eb2e34943e0bfbdc6a745fb9
mahakpandey <98814352+mahakpandey@users.noreply.github.com>
2025-08-01 16:54:43 +0530

    Implement cocktails section

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,18 @@
-import gsap, { ScrollTrigger,SplitText } from "gsap/all"
-import Navbar from "./components/Navbar"
-import Hero from "./components/Hero"
+import gsap, { ScrollTrigger, SplitText } from "gsap/all";
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import Cocktails from "./components/Cocktails";
 
-gsap.registerPlugin(ScrollTrigger, SplitText)
+gsap.registerPlugin(ScrollTrigger, SplitText);
 
 const App = () => {
   return (
     <main>
-        <Navbar />
-        <Hero />
-        <div className="h-dvh bg-black"></div>
+      <Navbar />
+      <Hero />
+      <Cocktails />
     </main>
-  )
-}
+  );
+};
 
-export default App
+export default App;
